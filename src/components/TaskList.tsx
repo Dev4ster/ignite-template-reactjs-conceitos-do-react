50fc757b313d0c0d0cb4d6f6b8291a04c51e09da
Victor Menezes Barreto <victormenezes35@hotmail.com>
2021-08-09 20:09:56 -0300

    refactor: implement methods of the test

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,28 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
+    if(newTaskTitle) {
+      const newTask: Task =  {
+        id: Math.random() + new Date().getTime(),
+        title: newTaskTitle,
+        isComplete: false
+      }
+      setTasks(oldTasks => [...oldTasks, newTask])
+    }
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
   }
 
   function handleToggleTaskCompletion(id: number) {
+    setTasks(oldTasks => oldTasks.map(task =>  ({
+      ...task,
+      isComplete: task.id === id ? !task.isComplete : task.isComplete
+    })))
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks(oldTasks => oldTasks.filter(task => task.id !== id) )
   }
 
   return (
